909a6cd46a2b6cdbcaece65b658c484e221360a2
RAParkinson <57231243+RAParkinson@users.noreply.github.com>
2019-11-01 17:44:41 +0000

    2. Reading From RichTextBox's and TextBox's
    Reading from both RichTextBox's and TextBox's so user can enter simple commands. Also creation of the BitMap's and paint methods.

---
+++ ASE/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ASE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(2, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 2);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Graphical Programming Language Application";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(375, 418);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 530);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(379, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ASE - Graphical Programming Language Application";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 502);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(367, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter commands in the box below and click submit:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 562);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 51);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SUBMIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(410, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 643);
+            this.panel1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Robert Andrew Parkinson - C3475586";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 624);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "LOAD";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 624);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 31);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "SAVE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1206, 667);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ASE - GPLA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ ASE/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASE
+{
+    public partial class Form1 : Form
+    {
+        Bitmap myBitmap;
+        string temp = "", command = "";
+        int x = 0, y = 0, width = 0, height = 0;
+        Point[] points = new Point[3];
+        int point1x = 0, point2x = 0, point3x = 0;
+        int point1y = 0, point2y = 0, point3y = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            myBitmap = new Bitmap(Size.Width, Size.Height);
+            Graphics g = Graphics.FromImage(myBitmap);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(myBitmap, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RichTextBox hiddenRTB = new RichTextBox();
+
+            if (textBox1.Text == "run")
+            {
+                hiddenRTB = richTextBox1;
+            }
+            else
+            {
+                hiddenRTB.AppendText(textBox1.Text);
+            }
+
+            foreach (var line in hiddenRTB.Lines)
+            {
+                temp = line;
+                temp = temp.ToLower();
+                temp = temp.Replace(" ", "");
+
+                try
+                {
+                    String[] sSplit = temp.Split(',');
+                    command = sSplit[0];
+
+                    if (command == "moveto")
+                    {
+                        MessageBox.Show("in moveto");
+                    }
+                    if (command == "drawto")
+                    {
+                        MessageBox.Show("in drawto");
+                    }
+                    if (command == "clear")
+                    {
+                        MessageBox.Show("in clear");
+                    }
+                    if (command == "reset")
+                    {
+                        MessageBox.Show("in reset");
+                    }
+                    //Shapes
+                    if (command == "rectangle")
+                    {
+                        MessageBox.Show("in rectangle");
+                    }
+                    if (command == "circle")
+                    {
+                        MessageBox.Show("in circle");
+                    }
+                    if (command == "triangle")
+                    {
+                        MessageBox.Show("in triangle");
+                    }
+                    else
+                    {
+                        //MessageBox.Show("Error when entering commands.");
+                    }
+                }
+                catch (Exception i)
+                {
+                    MessageBox.Show("Error when entering parameters.");
+                }
+
+                textBox1.Text = "";
+                richTextBox1.Text = "";
+            }
+        }
+    }
+}
