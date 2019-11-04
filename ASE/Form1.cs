3ed574562695be7dca218f675b7e1077dfdf1f26
RAParkinson <57231243+RAParkinson@users.noreply.github.com>
2019-11-04 10:04:38 +0000

    6. Documentation
    Completed documentation on code completed so far

--- ASE/Form1.cs
+++ ASE/Form1.cs
@@ -13,6 +13,7 @@ namespace ASE
 {
     public partial class Form1 : Form
     {
+        //Declares global variables and assigns them a value
         Bitmap myBitmap;
         string temp = "", command = "";
         int x = 0, y = 0, width = 0, height = 0;
@@ -21,14 +22,18 @@ namespace ASE
         int point1y = 0, point2y = 0, point3y = 0;
         RichTextBox hiddenRTB = new RichTextBox();
 
+        //Event handler for the Load button
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("*Feature coming soon*");
+            //Displays message box to the user
+            MessageBox.Show("*Load feature coming soon*");
         }
 
+        //Event handler for the Save button
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("*Feature coming soon*");
+            //Displays message box to the user
+            MessageBox.Show("*Save feature coming soon*");
         }
 
         public Form1()
@@ -39,17 +44,24 @@ namespace ASE
             Graphics g = Graphics.FromImage(myBitmap);
         }
 
+        //Event handler for painting the panel
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.DrawImageUnscaled(myBitmap, 0, 0);
         }
 
+        //Event handler for the Submit button
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //Reads user entered text from the text box
             if (textBox1.Text == "run")
             {
                 hiddenRTB = richTextBox1;
+            }
+            if(textBox1.Text == "")
+            {
+                //Displays message box to the user
+                MessageBox.Show("No command entered, please try again.");
             }
             else
             {
@@ -59,52 +71,69 @@ namespace ASE
             foreach (var line in hiddenRTB.Lines)
             {
                 temp = line;
+                //Sets all characters to lowercase 
                 temp = temp.ToLower();
+                //Removes spaces
                 temp = temp.Replace(" ", "");
 
                 try
                 {
+                    //Splits string by the , character
                     String[] sSplit = temp.Split(',');
                     command = sSplit[0];
 
-                    if (command == "moveto")
+                    if (command.Equals("moveto"))
                     {
+                        //Converts string into integer and assigns the value
                         x = Int32.Parse(sSplit[1]);
                         y = Int32.Parse(sSplit[2]);
                     }
-                    if (command == "drawto")
+                    if (command.Equals("drawto"))
                     {
+                        //Converts string into integer and assigns the value
                         width = Int32.Parse(sSplit[1]);
                         height = Int32.Parse(sSplit[2]);
+                        
+                        //Calls the DrawTo method
                         DrawTo();
                     }
-                    if (command == "clear")
+                    if (command.Equals("clear"))
                     {
+                        //Sets the bitmap colour to white
                         Graphics g = Graphics.FromImage(myBitmap);
                         g.Clear(Color.White);
+                        
+                        //Calls the Refresh method
                         Refresh();
                     }
-                    if (command == "reset")
+                    if (command.Equals("reset"))
                     {
+                        //Assigns value to x and y
                         x = 0;
                         y = 0;
                     }
                     //Shapes
-                    if (command == "rectangle")
+                    if (command.Equals("rectangle"))
                     {
+                        //Converts string into integer and assigns the value
                         width = Int32.Parse(sSplit[1]);
                         height = Int32.Parse(sSplit[2]);
-                        Rectangle();
 
+                        //Calls the Rectangle method
+                        Rectangle();
                     }
-                    if (command == "circle")
+                    if (command.Equals("circle"))
                     {
+                        //Converts string into integer and assigns the value
                         width = Int32.Parse(sSplit[1]);
                         height = Int32.Parse(sSplit[2]);
+                        
+                        //Calls the Circle method
                         Circle();
                     }
-                    if (command == "triangle")
+                    if (command.Equals("triangle"))
                     {
+                        //Converts string into integer and assigns the value
                         point1x = Int32.Parse(sSplit[1]);
                         point1y = Int32.Parse(sSplit[2]);
                         point2x = Int32.Parse(sSplit[3]);
@@ -115,20 +144,27 @@ namespace ASE
                         points[0] = new Point(point1x, point1y);
                         points[1] = new Point(point2x, point2y);
                         points[2] = new Point(point3x, point3y);
+                        
+                        //Calls the Triangle method
                         Triangle();
                     }
                     else
                     {
-                        //MessageBox.Show("Error when entering commands.");
+                        //MessageBox.Show("No valid command enter, please try again.");
                     }
                 }
-                catch (Exception i)
+                catch (FormatException)
                 {
-                    MessageBox.Show("Error when entering parameters.");
+                    //Displays message box to the user
+                    MessageBox.Show("Error when entering parameter, please try again.");
                 }
 
+                //Sets values for both richTextBox and textBox
                 textBox1.Text = "";
                 richTextBox1.Text = "";
+                hiddenRTB.Text = "";
+
+                //Calls the Refresh method
                 Refresh();
             }
         }
---
+++ ASEUnitTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ASEUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test to see if the system correctly set string to lowercase
+        [TestMethod]
+        public void TestLowerCase()
+        {
+
+        }
+
+        //Test to see if the system correctly set string to lowercase *suppose to fail*
+        [TestMethod]
+        public void TestUpperCase()
+        {
+
+        }
+
+        //Test to see if the system correctly removes the spaces from a string
+        [TestMethod]
+        public void TestRemovedSpaces()
+        {
+
+        }
+
+        //Test to see if the system correctly splits the string
+        [TestMethod]
+        public void TestSplitter()
+        {
+
+        }
+
+        //Test to see if the system correctly converts string to int
+        [TestMethod]
+        public void TestInt()
+        {
+
+        }
+    }
+}
