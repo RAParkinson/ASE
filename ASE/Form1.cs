16c1eebb7ab0950bdcf8aaa1bf2c7c02b32d3e84
RAParkinson <57231243+RAParkinson@users.noreply.github.com>
2019-12-31 00:01:34 +0000

    14. Loop
    Inserted if statements for the loop function. This involves allowing the user to set the count variable as well as where the loop will begin and end.

--- ASE/Form1.cs
+++ ASE/Form1.cs
@@ -27,6 +27,12 @@ namespace ASE
         int point1y = 0, point2y = 0, point3y = 0;
         RichTextBox hiddenRTB = new RichTextBox();
 
+        int tempRadius = 0; int tempWidth = 0; int tempHeight = 0;
+
+        int loopLineNum = 0;
+
+        int userCount = 0; int loopCount = 1;
+
         //Event handler for the Load button
         private void button2_Click(object sender, EventArgs e)
         {
@@ -192,6 +198,7 @@ namespace ASE
                         //Calls the Triangle method
                         shape.Triangle();
                     }
+                    //Parameters
                     else if (command.StartsWith("radius="))
                     {
                         String[] sSplitRE = command.Split('=');
@@ -255,6 +262,12 @@ namespace ASE
 
                         height = height + heightSubtract;
                     }
+                    else if (command.StartsWith("count="))
+                    {
+                        String[] sSplitCount = command.Split('=');
+                        userCount = Int32.Parse(sSplitCount[1]);
+                    }
+                    //If
                     else if (command.StartsWith("if"))
                     {
                         lineArray[i] = lineArray[i].Remove(0, 2);
@@ -271,6 +284,53 @@ namespace ASE
 
                             ifCOunt++;
                             i++;
+                        }
+
+                        if (ifArray[0].StartsWith("radius="))
+                        {
+                            String[] sSplitTempR = ifArray[0].Split('=');
+
+                            tempRadius = Int32.Parse(sSplitTempR[1]);
+                        }
+                        if (ifArray[0].StartsWith("width="))
+                        {
+                            String[] sSplitTempR = ifArray[0].Split('=');
+
+                            tempWidth = Int32.Parse(sSplitTempR[1]);
+                        }
+                        if (ifArray[0].StartsWith("height="))
+                        {
+                            String[] sSplitTempR = ifArray[0].Split('=');
+
+                            tempHeight = Int32.Parse(sSplitTempR[1]);
+                        }
+                    }
+                    //Loop
+                    else if (command.StartsWith("loop"))
+                    {
+                        String[] sSplitLoop = lineArray[i].Split(new string[] { "for" }, StringSplitOptions.None);
+
+                        MessageBox.Show(sSplitLoop[0]);
+                        MessageBox.Show(sSplitLoop[1]);
+
+                        int endloopLineNum = hiddenRTB.GetLineFromCharIndex(hiddenRTB.Find("endloop"));
+
+                        loopLineNum = i;
+                    }
+                    else if (command.Equals("endif"))
+                    {
+                        if (userCount != loopCount)
+                        {
+                            i = loopLineNum;
+                            loopCount++;
+                        }
+                        else if (userCount == loopCount)
+                        {
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("No valid count entered");
                         }
                     }
                     else
@@ -291,136 +351,6 @@ namespace ASE
                 Refresh();
                 i++;
             }
-
-            /*foreach (var line in hiddenRTB.Lines)
-            {
-                temp = line;
-                //Sets all characters to lowercase 
-                temp = temp.ToLower();
-                //Removes spaces
-                temp = temp.Replace(" ", "");
-
-                try
-                {
-                    //Splits string by the , character
-                    String[] sSplit = temp.Split(',');
-                    command = sSplit[0];
-
-                    if (command.Equals("moveto"))
-                    {
-                        //Converts string into integer and assigns the value
-                        x = Int32.Parse(sSplit[1]);
-                        y = Int32.Parse(sSplit[2]);
-                    }
-                    else if (command.Equals("drawto"))
-                    {
-                        //Converts string into integer and assigns the value
-                        width = Int32.Parse(sSplit[1]);
-                        height = Int32.Parse(sSplit[2]);
-
-                        //Calls the DrawTo method
-                        shape.DrawTo(x, y, height, width);
-                    }
-                    else if (command.Equals("clear"))
-                    {
-                        //Sets the bitmap colour to white
-                        Graphics g = Graphics.FromImage(shape.myBitmap);
-                        g.Clear(Color.White);
-                        
-                        //Calls the Refresh method
-                        Refresh();
-                    }
-                    else if (command.Equals("reset"))
-                    {
-                        //Assigns value to x and y
-                        x = 0;
-                        y = 0;
-                    }
-                    //Shapes
-                    else if (command.Equals("rectangle"))
-                    {
-                        //Converts string into integer and assigns the value
-                        width = Int32.Parse(sSplit[1]);
-                        height = Int32.Parse(sSplit[2]);
-
-                        //Calls the Rectangle method
-                        shape.Rectangle(x, y, height, width);
-                    }
-                    else if (command.Equals("circle"))
-                    {
-                        //Converts string into integer and assigns the value
-                        radius = Int32.Parse(sSplit[1]);
-
-                        //Calls the Circle method
-                        shape.Circle(x, y, radius);
-                    }
-                    else if (command.Equals("triangle"))
-                    {
-                        //Converts string into integer and assigns the value
-                        point1x = Int32.Parse(sSplit[1]);
-                        point1y = Int32.Parse(sSplit[2]);
-                        point2x = Int32.Parse(sSplit[3]);
-                        point2y = Int32.Parse(sSplit[4]);
-                        point3x = Int32.Parse(sSplit[5]);
-                        point3y = Int32.Parse(sSplit[6]);
-
-                        points[0] = new Point(point1x, point1y);
-                        points[1] = new Point(point2x, point2y);
-                        points[2] = new Point(point3x, point3y);
-
-                        //Calls the Triangle method
-                        shape.Triangle();
-                    }
-                    else if (command.StartsWith("if"))
-                    {
-                        command = command.Remove(0, 2);
-
-                        int endifLineNum = hiddenRTB.GetLineFromCharIndex(hiddenRTB.Find("endif"));
-
-                        MessageBox.Show("Line number: " +endifLineNum);
-
-                        String[] sSplitIf = command.Split('=');
-
-                        string ifVariable = sSplitIf[0];
-                        int ifValue = Int32.Parse(sSplitIf[1]);
-
-                        if (ifVariable.Equals("width"))
-                        {
-                            width = ifValue;
-                        }
-                        else if (ifVariable.Equals("height"))
-                        {
-                            height = ifValue;
-                        }
-                        else if (ifVariable.Equals("radius"))
-                        {
-                            radius = ifValue;
-                        }
-                        else
-                        {
-                            MessageBox.Show("No valid variable entered");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("No valid command entered, please try again.");
-                    }
-                }
-                catch (FormatException)
-                {
-                    //Displays message box to the user
-                    MessageBox.Show("Error when entering parameter, please try again.");
-                }
-
-                //Sets values for both richTextBox and textBox
-                textBox1.Text = "";
-                richTextBox1.Text = "";
-
-                lineNum++;
-
-                //Calls the Refresh method
-                Refresh();
-            } */
         }
     }
 }
